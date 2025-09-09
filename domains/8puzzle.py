bf895e12a5319ae217b4bd7e8108e6c8dced04c5
Creed Maxwell <creedmaxwell1@gmail.com>
2025-09-08 19:21:00 -0600

    problem and search

---
+++ domains/8puzzle.py
@@ -0,0 +1,80 @@
+class Puzzle:
+    def __init__(self):
+        self.goal_state = ((1,2,3),
+                           (4,5,6),
+                           (7,8,9))
+        
+    def InitialState(self):
+        return ((7,2,4),
+                (5,0,6),
+                (8,3,1))
+
+    def Actions(self, state):
+        actions = []
+        blank_row, blank_col = None, None
+        for i in range(3):
+            for j in range(3):
+                if state[i][j] == 0:
+                    blank_row, blank_col = i, j
+                    break
+        
+        possible_moves = [
+            ('up', (-1, 0)),
+            ('down', (1,0)),
+            ('left', (0,-1)),
+            ('right', (0,1))
+        ]
+
+        for move, (dr, dc) in possible_moves:
+            new_row, new_col = blank_row + dr, blank_col + dc
+            if 0 <= new_row < 3 and 0 <= new_col < 3:
+                actions.append(move)
+        
+        return actions
+
+    def Transition(self, state, action):
+        blank_row, blank_col = None, None
+        for i in range(3):
+            for j in range(3):
+                if state[i][j] == 0:
+                    blank_row, blank_col = i, j
+                    break
+        
+        moves = {
+            'up': (-1, 0),
+            'down': (1, 0),
+            'left': (0, -1),
+            'right': (0, 1)
+        }
+
+        dr, dc = moves[action]
+        new_row, new_col = blank_row + dr, blank_col + dc
+
+        new_state = [list(row) for row in state]
+        
+        new_state[blank_row][blank_col], new_state[new_row][new_col] = new_state[new_row][new_col], new_state[blank_row][blank_col]
+
+        return tuple(tuple(row) for row in new_state)
+
+    def GoalTest(self, state):
+        return state == self.goal_state
+
+    def StepCost(self, state, action, next_state):
+        return 1
+
+    def Heuristic(self, state):
+        #manhattan distance
+        distance = 0
+        goal_positions = {}
+        for i in range(3):
+            for j in range(3):
+                if self.goal_state[i][j] != 0:
+                    goal_positions[self.goal_state[i][j]] = (i,j)
+
+        for i in range(3):
+            for j in range(3):
+                if state[i][j] != 0:
+                    goal_i, goal_j = goal_positions[state[i][j]]
+                    distance += abs(i - goal_i) + abs(j- goal_j)
+        
+        return distance
---
+++ search_core.py
@@ -0,0 +1,61 @@
+import heapq
+
+class Node:
+    def __init__(self, state, parent=None, action=None, g=0, f=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.g = g
+        self.f = f
+        self.depth = 0 if parent is None else parent.depth + 1
+
+class Stats:
+    def __init__(self):
+        self.nodes_generated = 0
+        self.nodes_expanded = 0
+        self.max_frontier = 0
+        self.solution_cost = 0
+        self.solution_depth = 0
+
+def a_star(problem, initial_state):
+    stats = Stats()
+
+    h0 = problem.Heuristic(initial_state)
+    start_node = Node(initial_state, None, None, g=0, f=h0)
+
+    frontier = []
+    heapq.heappush(frontier, start_node)
+    frontier_dict = {initial_state: start_node}
+
+    best_g = {initial_state: 0}
+
+    while frontier:
+        stats.max_frontier = max(stats.max_frontier, len(frontier))
+
+        n = heapq.heappop(frontier)
+        del frontier_dict[n.state]
+        stats.nodes_expanded += 1
+
+        if problem.GoalTest(n.state):
+            stats.solution_cost = n.g
+            stats.solution_depth = n.depth
+            return n, stats
+        
+        for action in problem.Actions(n.state):
+            stats.nodes_generated += 1
+            s_prime = problem.Transition(n.state, action)
+            g_prime = n.g + problem.StepCost(n.state, action, s_prime)
+
+            if s_prime not in best_g or g_prime < best_g[s_prime]:
+                best_g[s_prime] = g_prime
+                f_prime = g_prime + problem.Heuristic(s_prime)
+                s_node = Node(s_prime, n, action, g_prime, f_prime)
+
+                if s_prime in frontier_dict:
+                    old_node = frontier_dict[s_prime]
+                    old_node.f = float('inf')
+                
+                heapq.heappush(frontier, (s_node.f, id(s_node), s_node))
+                frontier_dict[s_prime] = s_node
+    
+    return None, stats
